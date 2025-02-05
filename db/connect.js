12448f21c5d72e17095838ad45b1a3a6a6d511ce
viktorijakm <kmviktorija@gmail.com>
2025-02-04 19:40:38 -0500

    added background image to expense tracker

--- db/connect.js
+++ db/connect.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose')
 
 const connectDB = (url) => {
   return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    
   })
 }
 
